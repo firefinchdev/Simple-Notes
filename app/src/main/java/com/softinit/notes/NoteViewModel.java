84cca978bf115782426a5d250168bb7be185e260
Anirudh Gupta <firefinchdev@gmail.com>
2018-12-31 05:08:19 +0530

    adding view model

---
+++ app/src/main/java/com/softinit/notes/NoteViewModel.java
@@ -0,0 +1,43 @@
+package com.softinit.notes;
+
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class NoteViewModel extends AndroidViewModel {
+    // Extending AndroidViewModel but not ViewModel because we want Application and its context
+    // We should not pass context of activity as view model has to outlive the activity (doing this can cause memory leaks)
+
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
+    }
+
+    public void insert(Note note) {
+        repository.insert(note);
+    }
+
+    public void update(Note note) {
+        repository.update(note);
+    }
+
+    public void delete(Note note) {
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes() {
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+}
