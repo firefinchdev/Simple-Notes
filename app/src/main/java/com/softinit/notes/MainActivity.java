22768223e0b553fcfc56f1286b28fdde7af7bffb
Anirudh Gupta <firefinchdev@gmail.com>
2018-12-31 05:22:38 +0530

    Binding view model to activity

--- app/src/main/java/com/softinit/notes/MainActivity.java
+++ app/src/main/java/com/softinit/notes/MainActivity.java
@@ -1,13 +1,30 @@
 package com.softinit.notes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(MainActivity.this, "onChanged" , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
